fb3bec282ebce09aea1e62acfb65a0483fa0bd74
Kihoon Han <khhan1993@gmail.com>
2014-10-29 18:23:36 +0900

    Change Something

--- index.php
+++ index.php
@@ -51,8 +51,8 @@
 			<div class="navbar-collapse collapse">
 				<ul class="nav navbar-nav">
 					<li class="active"><a href="#">Home</a></li>
-					<li><a href="#about">About</a></li>
-					<li><a href="#contact">Contact</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Contact</a></li>
 					<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
 					<ul class="dropdown-menu">
@@ -67,9 +67,9 @@
 					</li>
 				</ul>
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="../navbar/">Default</a></li>
-					<li><a href="../navbar-static-top/">Static top</a></li>
-					<li class="active"><a href="./">Fixed top</a></li>
+					<li><a href="#">Default</a></li>
+					<li><a href="#">Static top</a></li>
+					<li class="active"><a href="#">Fixed top</a></li>
 				</ul>
 			</div>
 		</div>
