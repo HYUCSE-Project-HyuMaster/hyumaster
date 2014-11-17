896e0dbff03a155f01041adc95cf17264f0681a1
Kihoon Han <khhan1993@gmail.com>
2014-11-17 18:50:48 +0900

    restore

--- test.php
+++ test.php
@@ -15,34 +15,25 @@
 <body>
 	<div id='map'></div>
 	<script>
-		var oHanyangUnivPoint = new nhn.api.map.LatLng(127, 37.5);
-		var defaultLevel = 6;
+		var oSeoulCityPoint = new nhn.api.map.LatLng(37.5675451, 126.9773356);
+		var defaultLevel = 11;
 		var oMap = new nhn.api.map.Map(document.getElementById('map'), { 
-						point : oHanyangUnivPoint,
+						point : oSeoulCityPoint,
 						zoom : defaultLevel,
-						enableWheelZoom : false,
+						enableWheelZoom : true,
 						enableDragPan : true,
-						enableDblClickZoom : true,
+						enableDblClickZoom : false,
 						mapMode : 0,
 						activateTrafficMap : false,
 						activateBicycleMap : false,
-<<<<<<< HEAD
 						minMaxLevel : [ 1, 14 ],
-						size : new nhn.api.map.Size(800, 480)		
-					});
-=======
-						minMaxLevel : [ 1, 6 ],
-						size : new nhn.api.map.Size(800, 480),
-						detectCoveredMarker : true
-						});
->>>>>>> FETCH_HEAD
+						size : new nhn.api.map.Size(800, 480)		});
 		var oSlider = new nhn.api.map.ZoomControl();
 		oMap.addControl(oSlider);
 		oSlider.setPosition({
 			top : 10,
 			left : 10
 		});
-
 		var oMapTypeBtn = new nhn.api.map.MapTypeBtn();
 		oMap.addControl(oMapTypeBtn);
 		oMapTypeBtn.setPosition({
@@ -56,23 +47,36 @@
 			right : 10
 		});
 		oMap.addControl(oThemeMapBtn);
-
+		var oBicycleGuide = new nhn.api.map.BicycleGuide(); // - 자전거 범례 선언
+		oBicycleGuide.setPosition({
+			top : 10,
+			right : 10
+		}); // - 자전거 범례 위치 지정
+		oMap.addControl(oBicycleGuide);// - 자전거 범례를 지도에 추가.
+		var oTrafficGuide = new nhn.api.map.TrafficGuide(); // - 교통 범례 선언
+		oTrafficGuide.setPosition({
+			bottom : 30,
+			left : 10
+		});  // - 교통 범례 위치 지정.
+		oMap.addControl(oTrafficGuide); // - 교통 범례를 지도에 추가.
+		var trafficButton = new nhn.api.map.TrafficMapBtn(); // - 실시간 교통지도 버튼 선언
+		trafficButton.setPosition({
+			bottom:10, 
+			right:150
+		}); // - 실시간 교통지도 버튼 위치 지정
+		oMap.addControl(trafficButton);
 		var oSize = new nhn.api.map.Size(28, 37);
 		var oOffset = new nhn.api.map.Size(14, 37);
 		var oIcon = new nhn.api.map.Icon('//static.naver.com/maps2/icons/pin_spot2.png', oSize, oOffset);
-
 		var oInfoWnd = new nhn.api.map.InfoWindow();
 		oInfoWnd.setVisible(false);
 		oMap.addOverlay(oInfoWnd);
-
 		oInfoWnd.setPosition({
 			top : 20,
 			left :20
 		});
-
 		var oLabel = new nhn.api.map.MarkerLabel(); // - 마커 라벨 선언.
 		oMap.addOverlay(oLabel); // - 마커 라벨 지도에 추가. 기본은 라벨이 보이지 않는 상태로 추가됨.
-
 		oInfoWnd.attach('changeVisible', function(oCustomEvent) {
 			if (oCustomEvent.visible) {
 				oLabel.setVisible(false);
@@ -85,9 +89,7 @@
 			strokeOpacity : 0.5 // - 선의 투명도
 		}); // - polyline 선언, 첫번째 인자는 선이 그려질 점의 위치. 현재는 없음.
 		oMap.addOverlay(oPolyline); // - 지도에 선을 추가함.
-
 		oMap.attach('mouseenter', function(oCustomEvent) {
-
 			var oTarget = oCustomEvent.target;
 			// 마커위에 마우스 올라간거면
 			if (oTarget instanceof nhn.api.map.Marker) {
@@ -95,16 +97,13 @@
 				oLabel.setVisible(true, oMarker); // - 특정 마커를 지정하여 해당 마커의 title을 보여준다.
 			}
 		});
-
 		oMap.attach('mouseleave', function(oCustomEvent) {
-
 			var oTarget = oCustomEvent.target;
 			// 마커위에서 마우스 나간거면
 			if (oTarget instanceof nhn.api.map.Marker) {
 				oLabel.setVisible(false);
 			}
 		});
-
 		oMap.attach('click', function(oCustomEvent) {
 			var oPoint = oCustomEvent.point;
 			var oTarget = oCustomEvent.target;
@@ -132,7 +131,6 @@
 			var oMarker = new nhn.api.map.Marker(oIcon, { title : '마커 : ' + oPoint.toString() });
 			oMarker.setPoint(oPoint);
 			oMap.addOverlay(oMarker);
-
 			var aPoints = oPolyline.getPoints(); // - 현재 폴리라인을 이루는 점을 가져와서 배열에 저장.
 			aPoints.push(oPoint); // - 추가하고자 하는 점을 추가하여 배열로 저장함.
 			oPolyline.setPoints(aPoints); // - 해당 폴리라인에 배열에 저장된 점을 추가함
