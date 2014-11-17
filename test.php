9673f001ce6250ef8b8d42a3dcd9c751cb196be3
Kihoon Han <khhan1993@gmail.com>
2014-11-17 16:36:31 +0900

    test

--- test.php
+++ test.php
@@ -16,10 +16,6 @@
 	<script>
 
 		nhn.api.map.Map(map : "Test", {
-			point : Coord // 지도 중심점의 좌표
-			zoom : 5 // 지도의 축척 레벨
-			boundary : ['1','2']// 지도 생성 시 주어진 array 에 있는 점이 모두 보일 수 있도록 지도를 초기화한다.
-			boundaryOffset : 5 // boundary로 지도를 초기화 할 때 지도 전체에서 제외되는 영역의 크기.
 			enableWheelZoom : true // 마우스 휠 동작으로 지도를 확대/축소할지 여부
 			enableDragPan : true // 마우스로 끌어서 지도를 이동할지 여부
 			enableDblClickZoom : true // 더블클릭으로 지도를 확대할지 여부
