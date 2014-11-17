24d2f7840bfecd26e4f3a72fed1c39287b683252
Kihoon Han <khhan1993@gmail.com>
2014-11-17 16:24:15 +0900

    Map API test (11/17 16:24)

---
+++ test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>Map API test</title>
+	<meta charset='utf-8'>
+	<meta name='description' content='HyuMaster'>
+	<meta name='author' content='한기훈'>
+	<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>
+	<script src='/js/jquery-2.1.1.min.js'></script>
+	<script src='//openapi.map.naver.com/openapi/naverMap.naver?ver=2.0&key=311f555d1d9250996c08b0f69e8aee28'></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
