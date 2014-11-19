60eb12dfc8e9455abc4c440824ab537733b92499
Kihoon Han <khhan1993@gmail.com>
2014-11-19 14:19:35 +0900

    stest

--- index.php
+++ index.php
@@ -72,11 +72,27 @@
 				<ul class='nav navbar-nav navbar-right'>
 					<?php
 						if(isset($_SESSION['login_state']) && $_SESSION['login_state']==true)
-							echo "<li class='active'><a href='#' id='loginbutton' data-toggle='modal' data-target='#myModal'>로그아웃</a></li>";
+							echo "<li class='active'><a href='#' id='logoutbutton' data-toggle='modal' data-target='#myModal'>로그아웃</a></li>";
 						else
 							echo "<li class='active'><a href='#' id='loginbutton' data-toggle='modal' data-target='#myModal'>로그인</a></li>";
 					?>
 				</ul>
+
+				<script>
+					$(document).ready(function() {
+						$('#loginbutton').on({
+							click: function() {
+								alert('test');
+							}
+						});
+
+						$('#logoutbutton').on({
+							click: function() {
+
+							}
+						});
+					});
+				</script>
 			</div>
 			<!--네비게이션 바(메뉴) 부분 종료-->
 
@@ -90,42 +106,6 @@
 		</div>
 
 	</div>
-
-	<!--로그인 팝업창 소스 시작-->
-		<!-- Modal -->
-		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-						<h4 class="modal-title">Facebook Login</h4>
-					</div>
-
-					<div class="modal-body" id='login-state'>
-						
-					</div>
-
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
-					</div>
-				</div><!-- /.modal-content -->
-			</div><!-- /.modal-dialog -->
-		</div><!-- /.modal -->
-	<!--로그인 팝업창 소스 종료-->
-
-	<!--로그인 관련 팝업창 및 스크립트 시작-->
-	<script>
-		FB.getLoginStatus(function(response) {
-			if (response.status === 'connected') {
-				$('#login-state').html("로그인 상태입니다.");
-			}
-			else {
-				FB.login();
-			}
-		});
-	</script>
-	<!--로그인 관련 팝업창 및 스크립트 종료-->
 
 </body>
 
